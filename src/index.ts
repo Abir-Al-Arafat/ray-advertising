950968e8f66744487e5957c5dd03571f1c233d0f
abir <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-06-29 14:37:47 +0600

    add: response for base URL

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    name: "API Developer Skill Test",
+    developer: "Abir",
+    version: "1.0.0",
+    description: "Backend server for Skill Test",
+    status: "success",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
